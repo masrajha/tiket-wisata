02100932875621b2653d86eadd048e8c304fb6fa
masrajha <didikunila@gmail.com>
2023-02-24 19:56:57 +0700

    table

--- pesan-tiket.php
+++ pesan-tiket.php
@@ -15,72 +15,58 @@
 </head>
 
 <body>
-<?php
-$host = "localhost"; // host database
-$username = "sipuwebi_test"; // username database
-$password = "o0z98EI@;*ZB"; // password database
-$dbname = "sipuwebi_test"; // nama database
+    <?php
+    $host = "localhost"; // host database
+    $username = "sipuwebi_test"; // username database
+    $password = "o0z98EI@;*ZB"; // password database
+    $dbname = "sipuwebi_test"; // nama database
+    
+    // Membuat koneksi ke database
+    $conn = mysqli_connect($host, $username, $password, $dbname);
 
-// Membuat koneksi ke database
-$conn = mysqli_connect($host, $username, $password, $dbname);
+    // Mengecek apakah koneksi berhasil
+    if (!$conn) {
+        die("Koneksi gagal: " . mysqli_connect_error());
+    }
 
-// Mengecek apakah koneksi berhasil
-if (!$conn) {
-    die("Koneksi gagal: " . mysqli_connect_error());
-}
+    // Menyimpan data dari form ke dalam variabel
+    $nama_lengkap = $_POST["nama"];
+    $nomor_identitas = $_POST["identitas"];
+    $no_hp = $_POST["hp"];
+    $tempat_wisata = $_POST["wisata"];
+    $tanggal_kunjungan = $_POST["tanggal"];
+    $pengunjung_dewasa = $_POST["dewasa"];
+    $pengunjung_anak = $_POST["anak"];
+    $harga_tiket = $_POST["harga"];
+    $total_bayar = $_POST["total"];
 
-// Menyimpan data dari form ke dalam variabel
-$nama_lengkap = $_POST["nama"];
-$nomor_identitas = $_POST["identitas"];
-$no_hp = $_POST["hp"];
-$tempat_wisata = $_POST["wisata"];
-$tanggal_kunjungan = $_POST["tanggal"];
-$pengunjung_dewasa = $_POST["dewasa"];
-$pengunjung_anak = $_POST["anak"];
-$harga_tiket = $_POST["harga"];
-$total_bayar = $_POST["total"];
-
-// Menyimpan data ke dalam tabel pesan_tiket
-$sql = "INSERT INTO pesan_tiket (nama_lengkap, nomor_identitas, no_hp, tempat_wisata, tanggal_kunjungan, pengunjung_dewasa, pengunjung_anak, harga_tiket, total_bayar)
+    // Menyimpan data ke dalam tabel pesan_tiket
+    $sql = "INSERT INTO pesan_tiket (nama_lengkap, nomor_identitas, no_hp, tempat_wisata, tanggal_kunjungan, pengunjung_dewasa, pengunjung_anak, harga_tiket, total_bayar)
         VALUES ('$nama_lengkap', '$nomor_identitas', '$no_hp', '$tempat_wisata', '$tanggal_kunjungan', '$pengunjung_dewasa', '$pengunjung_anak', '$harga_tiket', '$total_bayar')";
 
-if (mysqli_query($conn, $sql)) {
-    displayData();
-    echo "<b>Data berhasil disimpan ke dalam tabel pesan_tiket</b>";
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-function displayData()
-{
-    echo "<div class='container'>";
-    echo "<div class='row'>";
-    echo "<div class='col-md-6'>";
-    echo "<p class='mb-0'>Nama lengkap :</p>";
-    echo "<p class='font-weight-bold'>" . $_POST["nama"] . "</p>";
-    echo "<p class='mb-0'>Nomor identitas :</p>";
-    echo "<p class='font-weight-bold'>" . $_POST["identitas"] . "</p>";
-    echo "<p class='mb-0'>No hp :</p>";
-    echo "<p class='font-weight-bold'>" . $_POST["hp"] . "</p>";
-    echo "<p class='mb-0'>Tempat wisata :</p>";
-    echo "<p class='font-weight-bold'>" . $_POST["wisata"] . "</p>";
-    echo "<p class='mb-0'>Tanggal kunjungan :</p>";
-    echo "<p class='font-weight-bold'>" . $_POST["tanggal"] . "</p>";
-    echo "</div>";
-    echo "<div class='col-md-6'>";
-    echo "<p class='mb-0'>Pengunjung dewasa :</p>";
-    echo "<p class='font-weight-bold'>" . $_POST["dewasa"] . "</p>";
-    echo "<p class='mb-0'>Pengunjung anak :</p>";
-    echo "<p class='font-weight-bold'>" . $_POST["anak"] . "</p>";
-    echo "<p class='mb-0'>Harga tiket :</p>";
-    echo "<p class='font-weight-bold'>Rp. " . $_POST["harga"] . "</p>";
-    echo "<p class='mb-0'>Total bayar :</p>";
-    echo "<p class='font-weight-bold'>Rp. " . $_POST["total"] . "</p>";
-    echo "</div>";
-    echo "</div>";
-    echo "</div>";
-}
-// Menutup koneksi
-mysqli_close($conn);
-?>
+    if (mysqli_query($conn, $sql)) {
+        displayData();
+        echo "<b>Data berhasil disimpan ke dalam tabel pesan_tiket</b>";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+    function displayData()
+    {
+        echo "<table class='table table-striped'>";
+        echo "<tr><th>Nama lengkap</th><td>" . $_POST["nama"] . "</td></tr>";
+        echo "<tr><th>Nomor identitas</th><td>" . $_POST["identitas"] . "</td></tr>";
+        echo "<tr><th>No hp</th><td>" . $_POST["hp"] . "</td></tr>";
+        echo "<tr><th>Tempat wisata</th><td>" . $_POST["wisata"] . "</td></tr>";
+        echo "<tr><th>Tanggal kunjungan</th><td>" . $_POST["tanggal"] . "</td></tr>";
+        echo "<tr><th>Pengunjung dewasa</th><td>" . $_POST["dewasa"] . "</td></tr>";
+        echo "<tr><th>Pengunjung anak-anak</th><td>" . $_POST["anak"] . "</td></tr>";
+        echo "<tr><th>Harga tiket</th><td>" . $_POST["harga"] . "</td></tr>";
+        echo "<tr><th>Total bayar</th><td>" . $_POST["total"] . "</td></tr>";
+        echo "</table>";
+    }
+    // Menutup koneksi
+    mysqli_close($conn);
+    ?>
 </body>
+
 </html>
