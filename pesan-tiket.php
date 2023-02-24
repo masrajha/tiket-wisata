e9e69bb5c18d2505196734ff6187d53b281327f9
masrajha <didikunila@gmail.com>
2023-02-24 18:38:51 +0700

    display data

--- pesan-tiket.php
+++ pesan-tiket.php
@@ -1,3 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wisata Lampung</title>
+
+    <!-- Tautan ke file CSS Bootstrap -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/css/bootstrap.min.css" />
+
+    <!-- Tautan ke file JavaScript Bootstrap -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
+
+</head>
+
+<body>
 <?php
 $host = "localhost"; // host database
 $username = "sipuwebi_test"; // username database
@@ -28,22 +45,42 @@ $sql = "INSERT INTO pesan_tiket (nama_lengkap, nomor_identitas, no_hp, tempat_wi
         VALUES ('$nama_lengkap', '$nomor_identitas', '$no_hp', '$tempat_wisata', '$tanggal_kunjungan', '$pengunjung_dewasa', '$pengunjung_anak', '$harga_tiket', '$total_bayar')";
 
 if (mysqli_query($conn, $sql)) {
-    echo "Nama_lengkap : ".$_POST["nama"]."</br>";
-    echo "Nomor_identitas : ".$_POST["identitas"]."</br>";
-    echo "No_hp : ".$_POST["hp"]."</br>";
-    echo "Tempat_wisata : ".$_POST["wisata"]."</br>";
-    echo "Tanggal_kunjungan : ".$_POST["tanggal"]."</br>";
-    echo "Pengunjung_dewasa : ".$_POST["dewasa"]."</br>";
-    echo "Pengunjung_anak : ".$_POST["anak"]."</br>";
-    echo "Harga_tiket : ".$_POST["harga"]."</br>";
-    echo "Total_bayar : ".$_POST["total"]."</br>";
-
-
+    displayData();
     echo "<b>Data berhasil disimpan ke dalam tabel pesan_tiket</b>";
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
-
+function displayData()
+{
+    echo "<div class='container'>";
+    echo "<div class='row'>";
+    echo "<div class='col-md-6'>";
+    echo "<p class='mb-0'>Nama lengkap :</p>";
+    echo "<p class='font-weight-bold'>" . $_POST["nama"] . "</p>";
+    echo "<p class='mb-0'>Nomor identitas :</p>";
+    echo "<p class='font-weight-bold'>" . $_POST["identitas"] . "</p>";
+    echo "<p class='mb-0'>No hp :</p>";
+    echo "<p class='font-weight-bold'>" . $_POST["hp"] . "</p>";
+    echo "<p class='mb-0'>Tempat wisata :</p>";
+    echo "<p class='font-weight-bold'>" . $_POST["wisata"] . "</p>";
+    echo "<p class='mb-0'>Tanggal kunjungan :</p>";
+    echo "<p class='font-weight-bold'>" . $_POST["tanggal"] . "</p>";
+    echo "</div>";
+    echo "<div class='col-md-6'>";
+    echo "<p class='mb-0'>Pengunjung dewasa :</p>";
+    echo "<p class='font-weight-bold'>" . $_POST["dewasa"] . "</p>";
+    echo "<p class='mb-0'>Pengunjung anak :</p>";
+    echo "<p class='font-weight-bold'>" . $_POST["anak"] . "</p>";
+    echo "<p class='mb-0'>Harga tiket :</p>";
+    echo "<p class='font-weight-bold'>Rp. " . $_POST["harga"] . "</p>";
+    echo "<p class='mb-0'>Total bayar :</p>";
+    echo "<p class='font-weight-bold'>Rp. " . $_POST["total"] . "</p>";
+    echo "</div>";
+    echo "</div>";
+    echo "</div>";
+}
 // Menutup koneksi
 mysqli_close($conn);
 ?>
+</body>
+</html>
